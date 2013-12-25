b46bebd17a8bc5b5ea1e2d13d9b0d024c60fdacf
Eremikhin Alexey <alexxz@alexxz.ru>
2013-12-26 02:21:03 +0400

    my first changes

--- src/com/mycompany/myapp/MainActivity.java
+++ src/com/mycompany/myapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import android.widget.ExpandableListView.*;
 
 public class MainActivity extends Activity
 {
@@ -13,5 +14,20 @@ public class MainActivity extends Activity
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		Button exitButton = (Button) findViewById(R.id.exitbutton);
+		exitButton.setOnClickListener( new OnClickListener () {
+			public void onClick(View p1) {
+				finish();
+			}
+		});
+		
+		Button NextQuoteButton = (Button) findViewById(R.id.nextquotebutton);
+		NextQuoteButton.setOnClickListener( new OnClickListener () {
+				public void onClick(View p1) {
+					TextView text = (TextView) findViewById(R.id.quotetext);
+					text.setText("My quote text");
+				}
+			});
     }
 }
