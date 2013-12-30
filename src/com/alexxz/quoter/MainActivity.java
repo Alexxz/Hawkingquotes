2da450c958c9741c92c0b3e3c7bb72b2d872878c
Eremikhin Alexey <alexxz@alexxz.ru>
2013-12-31 01:55:43 +0400

    add more quotes

--- src/com/alexxz/quoter/MainActivity.java
+++ src/com/alexxz/quoter/MainActivity.java
@@ -11,8 +11,16 @@ public class MainActivity extends Activity
 {
 	final static private String[] quotes = {
 		"We need something new. We can't predict what that will be or when we will find it because if we knew that, we would have found it already!",
-		"quote2",
-		"quote3"
+		"For millions of years, mankind lived just like the animals. Then something happened which unleashed the power of our imagination. We learned to talk and we learned to listen...",
+		"Consideration of black holes suggests, not only that God does play dice, but that he sometimes confuses us by throwing them where they can't be seen.",
+		"Many people would claim that the boundary conditions are not part of physics but belong to metaphysics or religion. They would claim that nature had complete freedom to start the universe off any way it wanted. That may be so, but it could also have made it evolve in a completely arbitrary and random manner. Yet all the evidence is that it evolves in a regular way according to certain laws.",
+		"If you are disabled, it is probably not your fault, but it is no good blaming the world or expecting it to take pity on you. One has to have a positive attitude and must make the best of the situation that one finds oneself in; if one is physically disabled, one cannot afford to be psychologically disabled as well.",
+		"My goal is simple. It is a complete understanding of the universe, why it is as it is and why it exists at all.",
+		"There ought to be something very special about the boundary conditions of the universe and what can be more special than that there is no boundary?",
+		"What I have done is to show that it is possible for the way the universe began to be determined by the laws of science. In that case, it would not be necessary to appeal to God to decide how the universe began. This doesn't prove that there is no God, only that God is not necessary.",
+		"We are just an advanced breed of monkeys on a minor planet of a very average star. But we can understand the Universe. That makes us something very special.",
+		"So Einstein was wrong when he said, \"God does not play dice.\" Consideration of black holes suggests, not only that God does play dice, but that he sometimes confuses us by throwing them where they can't be seen.",
+		"The human race is just a chemical scum on a moderate-sized planet, orbiting around a very average star in the outer suburb of one among a hundred billion galaxies. We are so insignificant that I can't believe the whole universe exists for our benefit. That would be like saying that you would disappear if I closed my eyes.",
 		};
 		
 	private GestureDetectorCompat mDetector; 
