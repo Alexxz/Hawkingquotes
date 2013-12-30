006233388fe4c80911cf7ad4f8f4ab30b150b805
Eremikhin Alexey <alexxz@alexxz.ru>
2013-12-31 01:37:43 +0400

    Add basic gestures support

--- src/com/alexxz/quoter/MainActivity.java
+++ src/com/alexxz/quoter/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.*;
 import android.view.*;
 import android.widget.*;
 import android.widget.ExpandableListView.*;
+import android.support.v4.view.*;
 
 public class MainActivity extends Activity
 {
@@ -13,12 +14,19 @@ public class MainActivity extends Activity
 		"quote2",
 		"quote3"
 		};
+		
+	private GestureDetectorCompat mDetector; 
+	
+	private int counter = 0;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		this.mDetector = new GestureDetectorCompat(this,  new MyGestureListener());
 		
 		final Button exitButton = (Button) findViewById(R.id.exitbutton);
 		exitButton.setOnClickListener( new OnClickListener () {
@@ -29,15 +37,42 @@ public class MainActivity extends Activity
 		
 		final Button NextQuoteButton = (Button) findViewById(R.id.nextquotebutton);
 		NextQuoteButton.setOnClickListener( new OnClickListener () {
-			    private int counter = 0;
 				
 				public void onClick(View p1) {
-					final TextView text = (TextView) findViewById(R.id.quotetext);
-					//NextQuoteButton.setText("");
-					text.setText(MainActivity.quotes[this.counter]);
-					this.counter++;
-					this.counter = this.counter % MainActivity.quotes.length;
-				}
+					 showNextQuote();
+                }
 			});
+    }
+	
+	private  void showNextQuote()
+	{
+		final TextView text = (TextView) findViewById(R.id.quotetext);
+		//NextQuoteButton.setText("");
+		text.setText(MainActivity.quotes[this.counter]);
+		this.counter++;
+		this.counter = this.counter % MainActivity.quotes.length;
+		
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event){ 
+	    this.mDetector.onTouchEvent(event);
+		return super.onTouchEvent(event);
+	}
+	
+	class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+		 
+		@Override
+	    public boolean onDown(MotionEvent event) { 
+		//Log.d(DEBUG_TAG,"onDown: " + event.toString()); 
+			return true;
+		}
+
+		@Override
+		public boolean onFling(MotionEvent event1, MotionEvent event2, 
+							  float velocityX, float velocityY) {
+		    showNextQuote();
+		    return true;
+		}
     }
 }
